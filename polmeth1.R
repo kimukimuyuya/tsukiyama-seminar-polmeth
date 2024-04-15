3bb9c816e7e86766966d89342acf405fbebd54b2
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-04-15 22:22:32 +0900

    賃金構造基本統計調査2017年と2022年の比較

--- polmeth1.R
+++ polmeth1.R
@@ -1,7 +1,10 @@
 wages <- read.csv("wages2017.csv", fileEncoding = "SJIS")
 mean(wages$Ｄ建設業, na.rm = TRUE)
 
+#分散の計算
 var(wages$Ｄ建設業, na.rm = TRUE)
+
+#標準偏差の計算
 sd(wages$Ｄ建設業, na.rm = TRUE)
 sd(wages$Ｇ情報通信業, na.rm = TRUE)
 
@@ -10,4 +13,8 @@ library(psych)
 describe(wages[, -1])
 
 res <- describe(wages[, -1])
-write.csv(res, "res.csv", fileEncoding = "SJIS")
+write.csv(res, "res.csv", fileEncoding = "SJIS", row.names = FALSE)
+
+wages2022 <- read.csv("wages2022.csv", fileEncoding = "SJIS")
+res2 <- describe(wages2022[, -1])
+write.csv(res2, "res2.csv", fileEncoding = "SJIS")
