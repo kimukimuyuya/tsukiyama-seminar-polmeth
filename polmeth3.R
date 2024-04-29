4fbec1d61490410878b9efa2b1026e1d7c70bc83
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-04-29 19:31:58 +0900

    クロス集計表とχ二乗検定

--- polmeth3.R
+++ polmeth3.R
@@ -9,6 +9,7 @@ tab <- addmargins(tab)
 
 rownames(tab) <- c("男性", "女性", "合計")
 colnames(tab) <- c("棄権", "投票", "合計")
+tab
 
 tab <- table(utasv$W1F1, utasv$W1Q1)
 chisq.test(tab, correct = FALSE)
@@ -18,3 +19,23 @@ tab <- CrossTable(utasv$W1F1, utasv$W1Q1,
                   prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE,
                   chisq = TRUE)
 tab
+
+#消費税増税に賛成する理由を分析
+tax <- utasv$W1Q16_8
+tax[utasv$W1Q16_8 == 1 | utasv$W1Q16_8 == 2] <- 1
+tax[utasv$W1Q16_8 == 4 | utasv$W1Q16_8 == 5] <- 0
+tax[utasv$W1Q16_8 == 3] <- NA
+
+values <- utasv$W1Q17_2
+values[utasv$W1Q17_2 == 1 | utasv$W1Q17_2 == 2] <- 0
+values[utasv$W1Q17_2 == 4 | utasv$W1Q17_2 == 5] <- 1
+values[utasv$W1Q17_2 == 3] <- NA
+
+tab <- table(values, tax)
+chisq.test(tab, correct = FALSE)
+
+tab <- CrossTable(values, tax,
+                   prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE,
+                   chisq = TRUE)
+tab
+write.csv(descr:::CreateNewTab(tab2), "polmeth3.csv")
