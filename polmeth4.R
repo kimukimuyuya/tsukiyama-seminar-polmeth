b0a29d3a88f0ae034cc043a396a0d8ce450c14f2
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-05-02 14:22:04 +0900

    大数の法則,中心極限定理のシミュレーション

---
+++ polmeth4.R
@@ -0,0 +1,37 @@
+x <- rep(NA, 5)
+
+for (i in 1:5) {
+  x[i] <- i * 2
+}
+x
+
+set.seed(12345)
+
+# 大数の法則
+n <- 5000
+sim <- rep(NA, n)
+
+for (i in 1:n) {
+  spl <- sample(c(1,0), i, replace = TRUE, prob = c(0.5, 0.5))
+  sim[i] <- mean(spl)
+}
+
+plot(1:n, sim, type = "l", ylim = c(0, 1), xlab = "標本サイズ", ylab = "標本平均")
+abline(h = 0.5, lty = "dotted", col = "red")
+
+# 中心極限定理
+n <-　100
+k <- 5000
+sim <- rep(NA, k)
+
+for (i in 1:k) {
+  spl <- sample(c(1,0), n, replace = TRUE, prob = c(0.5, 0.5))
+  sim[i] <- mean(spl)
+}
+
+hist(sim, freq = FALSE, breaks = seq(0.25, 0.75, 0.02),
+     xlim = c(0.3, 0.7), ylim = c(0, 8),
+     xlab = "標本平均", main = "")
+abline(v = 0.5, lty = "dotted", col = "red")
+x <- seq(from = 0.3, to = 0.7, by = 0.005)
+lines(x, dnorm(x, mean = 0.5, sd = sqrt(0.25 / n)), col = "blue")
