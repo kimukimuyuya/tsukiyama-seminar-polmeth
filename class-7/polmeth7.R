7ae5797684b48cf32cff1194c4e8b170f7a74b54
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-05-23 14:13:43 +0900

    対数化、二乗項

---
+++ class-7/polmeth7.R
@@ -0,0 +1,26 @@
+df <- read.csv("SSDSE-2019A.csv", fileEncoding = "SJIS")
+df <- subset(df, df$都道府県 != "福島県")
+
+df$一人当たり歳出総額 <- df$歳出決算総額 / df$総人口
+plot(df$総人口, df$一人当たり歳出総額, pch = 20)
+
+result1 <- lm(一人当たり歳出総額 ~ 総人口, data = df)
+summary(result1)
+
+
+#対数線形モデル
+plot(log(df$総人口), log(df$一人当たり歳出総額), pch = 20)
+result2 <- lm(log(一人当たり歳出総額) ~ log(総人口), data = df)
+summary(result2)
+
+result3 <- lm(log(一人当たり歳出総額) ~ log(総人口) + I(log(総人口)^2), data = df)
+summary(result3)
+
+plot(log(df$総人口), log(df$一人当たり歳出総額), pch = 20)
+b <- result3$coefficients
+x <- seq(5, 16, 0.01)
+y.hat <- b[1] + b[2] * x + b[3] * x^2
+lines(x, y.hat, col = "red")
+
+b[2]/(-2*b[3])
+exp(b[2]/(-2*b[3]))
