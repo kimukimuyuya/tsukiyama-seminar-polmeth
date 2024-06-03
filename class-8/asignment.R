6bea6bd3d98444533af6dfe3a48881318c7cf6fa
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-06-03 19:39:24 +0900

    二項ロジット分析の実行

---
+++ class-8/asignment.R
@@ -0,0 +1,16 @@
+utasp <- read.csv("utasp.csv", na.strings = c("99", "999"), fileEncoding = "SJIS")
+
+utasp <- subset(utasp, utasp$PR == 0)
+
+utasp$WIN <- 0
+utasp$WIN[utasp$RESULT == 2 | utasp$RESULT == 3] <- 1
+
+utasp$LDP <- 0
+utasp$LDP[utasp$PARTY == 1] <- 1
+
+utasp$AGE
+# 二項ロジット分析
+# result <- glm(WIN ~ TERM + LDP, data = utasp, family = binomial(link = "logit"))
+result <- glm(WIN ~ TERM + LDP + AGE + I(AGE^2), data = utasp, family = binomial(link = "logit"))
+summary(result)
+
