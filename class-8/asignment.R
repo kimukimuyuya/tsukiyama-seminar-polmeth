dfdf235a0848b6434a1b72a144c4e19947010e8e
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-06-03 19:59:08 +0900

    年齢による当選確率の変化をプロット

--- class-8/asignment.R
+++ class-8/asignment.R
@@ -8,9 +8,27 @@ utasp$WIN[utasp$RESULT == 2 | utasp$RESULT == 3] <- 1
 utasp$LDP <- 0
 utasp$LDP[utasp$PARTY == 1] <- 1
 
-utasp$AGE
 # 二項ロジット分析
 # result <- glm(WIN ~ TERM + LDP, data = utasp, family = binomial(link = "logit"))
 result <- glm(WIN ~ TERM + LDP + AGE + I(AGE^2), data = utasp, family = binomial(link = "logit"))
 summary(result)
+
+max(utasp$AGE)
+min(utasp$AGE)
+
+
+# 予測確立のプロット
+
+b <- coef(result)
+print(b)
+print(utasp$TERM)
+pr <- numeric()
+for (i in 25:80) {
+  prd <- exp(b[1] + b[2] * utasp$TERM + b[3] * utasp$LDP + b[4] * i + b[5] * I(i^2)) / (1 + exp(b[1] + b[2] * utasp$TERM + b[3] * utasp$LDP + b[4] * i + b[5] * I(i^2)))
+  mprd <- mean(prd, na.rm = T)
+  pr <- append(pr, mprd)
+}
+
+matplot(c(25:80), pr, type = "b", pch = 20, xlab = "年齢", ylab = "当選確率")
+
 
