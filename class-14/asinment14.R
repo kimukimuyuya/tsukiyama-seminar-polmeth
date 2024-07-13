84bb2cd366f8b7b9da91e4ee0b3431ed8af08429
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-07-13 18:45:12 +0900

    変数同士の相関関係の確認

---
+++ class-14/asinment14.R
@@ -0,0 +1,4 @@
+utasv_pca <- read.csv("utasv.csv", na.strings = c("66", "99", "NA"), fileEncoding = "SJIS")
+df1 <- utasv_pca[, c("W1Q16_1", "W1Q16_2", "W1Q16_3", "W1Q16_4", "W1Q16_5", "W1Q16_6", "W1Q16_7", "W1Q16_8", "W1Q16_9"
+, "W1Q16_10", "W1Q16_11", "W1Q16_12", "W1Q16_13", "W1Q16_14", "W1Q16_15", "W1Q16_16", "W1Q16_17")]
+res1 <- cor(df1, use = "pairwise.complete.obs")
